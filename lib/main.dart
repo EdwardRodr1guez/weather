4607b4beaca050c621c548800c48fbc45c1bc3b5
EdwardRodriguezPeacedev <edward.rodriguez@peacedev.co>
2023-07-06 23:31:21 -0500

    loader screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:weather/screens/weather_screen.dart';
+import 'package:weather/theme/app_theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  //localization permission is requested for fetching data
+  runApp(const MyApp()); //Run the app
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  /*
+  This widget is the root of your application. Material app wraps
+  the entire application. WeatherScreen is a default page. 
+  */
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
+        title: 'Weather',
+        theme: AppTheme().getTheme(),
         home: const WeatherScreen());
   }
 }
---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,99 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: _Loader());
+  }
+}
+
+class _Loader extends StatelessWidget {
+  const _Loader();
+
+  Stream<String> getLoadingMessages() {
+    final messages = <String>[
+      "Consultando el clima...",
+      "Un grande nubarrón se hace en el cielo \n Ya se aproxima una fuerte tormenta! ♪♫♪♫♪♫ ",
+      "Calculando el clima con algoritmos a prueba de lluvia...",
+      "Estamos barriendo las nubes para darte el pronóstico más preciso.",
+      ""
+    ];
+    return Stream.periodic(
+      const Duration(milliseconds: 1500),
+      (step) {
+        Future.delayed(const Duration(milliseconds: 300));
+        return messages[step];
+      },
+    ).take(messages.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+    final height = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30 * 0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image(
+            height: height,
+            width: double.infinity,
+            image: const AssetImage("assets/c.png"),
+            fit: BoxFit.fill,
+          ),
+          SizedBox.expand(
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        stops: const [0, 0.33, 0.66],
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent.withOpacity(0.1),
+                          Colors.transparent,
+                          Colors.transparent
+                        ]))),
+          ),
+          Positioned(
+            bottom: height * 0.7,
+            child: FadeInRight(
+              delay: const Duration(milliseconds: 500),
+              duration: const Duration(milliseconds: 1000),
+              child: Text(
+                "¡Bienvenido a tu App del clima!",
+                style: titleStyle,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.35,
+            child: const CircularProgressIndicator(
+              strokeWidth: 4,
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.28,
+            child: StreamBuilder(
+              stream: getLoadingMessages(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  Text("Cargando ...", style: captionStyle);
+                }
+                return Text(
+                  snapshot.data ?? "Cargando ...",
+                  style: captionStyle,
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
